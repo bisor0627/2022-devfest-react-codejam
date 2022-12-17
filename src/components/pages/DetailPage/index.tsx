3cf857a2076a397b87dc31e3b73ac674f69be7c7
biso <bisor0627@gmail.com>
2022-12-17 13:06:11 +0900

    ╬ô├¬ΓéºΓö£├╗Γö£├▒Γò¼ΓöñΓö£ΓòæΓö£┬½

--- src/components/pages/DetailPage/index.tsx
+++ src/components/pages/DetailPage/index.tsx
@@ -1,5 +1,17 @@
+import { AppScreen } from '@stackflow/plugin-basic-ui';
+import AppBar from '@stackflow/plugin-basic-ui/dist/components/AppBar';
 import React from 'react';
+import { DetailPageBackButton } from 'src/components/common/Stackflow';
+import { ProductInterface, UserInterface } from 'src/schemas/Product';
+import { getProductData } from 'src/services/product';
+import { getUserData } from 'src/services/user';
+import { useFlow } from 'src/utils/stackflow';
 import styled from 'styled-components';
+import OtherItems from './components/OtherItems';
+import { OtherItem } from './components/OtherItems/styled';
+import ProductDetail from './components/ProductDetail';
+import { ProductDetailWrapper } from './components/ProductDetail/styled';
+import UserProfile from './components/UserProfile';
 
 type DetailParams = {
   params: {
@@ -8,7 +20,33 @@ type DetailParams = {
 };
 
 const DetailPage: React.FC<DetailParams> = ({ params: { id } }) => {
-  return <ProductImageWrapper></ProductImageWrapper>;
+  const {pop} = useFlow();
+  const [product, setProduct] = React.useState<ProductInterface>();
+  const [user, setUser] = React.useState<UserInterface>(); 
+  const loadData =async () => {
+    const {data} = await getProductData(Number(id));
+
+    setProduct(data);
+    const {data: userData} = await getUserData(data.userID);
+
+    setUser(userData);
+  }
+
+  React.useEffect(() => {
+    loadData()
+  }, [])
+  return <AppScreen appBar={{backButton: {
+    render: ()=><DetailPageBackButton onClick={pop}></DetailPageBackButton>
+  }}}>
+    {product && user && (<section>
+      <ProductImageWrapper>
+        <img src={product.img} alt="productImage"/>
+      </ProductImageWrapper>
+<UserProfile {...user}/>
+      <ProductDetail {...product}/>
+       <OtherItems  userName={user.userName} other={user.other}/> 
+    </section>)}
+   </AppScreen>;
 };
 
 export default DetailPage;
--- src/components/pages/MainPage/index.tsx
+++ src/components/pages/MainPage/index.tsx
@@ -1,11 +1,17 @@
 import { AppScreen } from '@stackflow/plugin-basic-ui';
 import { ActivityComponentType } from '@stackflow/react';
 import React from 'react';
+import Footer from 'src/components/common/Footer';
+import ProductItem from 'src/components/common/ProductItem';
 import { MainPageAppBarLeft, MainPageAppBarRight } from 'src/components/common/Stackflow';
 import { ProductInterface } from 'src/schemas/Product';
 import { getProductList } from 'src/services/product';
+import { useFlow } from 'src/utils/stackflow';
+import { ItemsWrapper } from './styled';
 
 const MainPage: ActivityComponentType = () => {
+  const {push} = useFlow();
+  const goToDetailPage = (id:number)=>{push('DetailPage', {id: id.toString()})};
   const [products, setProducts] = React.useState<ProductInterface[]>([]);
   const loadProducts =async () => {
     const {data} = await getProductList();
@@ -13,7 +19,14 @@ const MainPage: ActivityComponentType = () => {
   }
 
   React.useEffect(()=>{loadProducts();},[]);
-  return <AppScreen appBar={{appendLeft: MainPageAppBarLeft, appendRight: MainPageAppBarRight}}><div></div>
+  return <AppScreen appBar={{appendLeft: MainPageAppBarLeft, appendRight: MainPageAppBarRight}}>
+    <div></div>
+  <ItemsWrapper >
+  {products.map((product) => {
+  return <ProductItem key = {product.id} item={product} onClickItem={()=>goToDetailPage(product.id)}/>
+  })}
+  </ItemsWrapper>
+  <Footer/>
   </AppScreen>;
 };
 
